a463db655c0ee3ad0c767cd0d00682929c7015b3
alaarmann <alaarmann@gmx.net>
2024-03-29 17:52:07 +0100

    Adapt magnetometer example

--- examples/magnetometer/src/main.rs
+++ examples/magnetometer/src/main.rs
@@ -1,66 +1,43 @@
 #![no_std]
 #![no_main]
 
+use bmx055::Bmx055;
+use calliope_mini::hal::{
+    pac::twi0::frequency::FREQUENCY_A,
+    prelude::*,
+    twi::{self},
+    Timer,
+};
 use defmt_rtt as _;
 use panic_halt as _;
 
 use cortex_m_rt::entry;
 
-use microbit::hal::{prelude::*, Timer};
-
-use microbit::{
-    hal::twim,
-    pac::{twim0::frequency::FREQUENCY_A, TWIM0},
-};
-
-use lsm303agr::{
-    interface::I2cInterface, mode::MagOneShot, AccelMode, AccelOutputDataRate, Lsm303agr,
-};
-
 #[entry]
 fn main() -> ! {
-    let board = microbit::Board::take().unwrap();
+    let board = calliope_mini::Board::take().unwrap();
+
+    let i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
+
     let mut timer = Timer::new(board.TIMER0);
 
-    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let mut sensor = Bmx055::new_with_i2c(i2c);
+    defmt::println!("Initializing...");
+    sensor.init(&mut timer).unwrap();
 
-    let mut sensor = Lsm303agr::new_with_i2c(i2c);
-    match sensor.accelerometer_id() {
-        Ok(0x33u8) => {}
-        _ => defmt::panic!("accelerometer not found"),
-    }
-    sensor.init().unwrap();
-    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
-
-    defmt::info!("normal mode");
-    sensor.set_accel_mode(AccelMode::Normal).unwrap();
-    timer.delay_ms(1000_u32);
-    get_data(&mut sensor);
-
-    defmt::info!("low power mode");
-    sensor.set_accel_mode(AccelMode::LowPower).unwrap();
-    timer.delay_ms(1000_u32);
-    get_data(&mut sensor);
-
-    defmt::info!("high resolution mode");
-    sensor.set_accel_mode(AccelMode::HighResolution).unwrap();
-    timer.delay_ms(1000_u32);
-    get_data(&mut sensor);
+    sensor.set_accel_bw(bmx055::AccelBandwidth::Hz125).unwrap();
+    defmt::println!("Initialized.");
 
     loop {
-        timer.delay_ms(100_u32);
-        get_data(&mut sensor);
-    }
-}
-
-type Sensor = Lsm303agr<I2cInterface<twim::Twim<TWIM0>>, MagOneShot>;
-
-fn get_data(sensor: &mut Sensor) {
-    loop {
-        if sensor.accel_status().unwrap().xyz_new_data {
-            let data = sensor.accel_data().unwrap();
-            defmt::info!("x {} y {} z {}", data.x, data.y, data.z);
-            return;
+        let magnetic_field_data = sensor.magnetic_field_data().unwrap();
+        if magnetic_field_data.data_ready() {
+            defmt::println!(
+                "Magnetic Field Data (in microTesla): x {} y {} z {}",
+                magnetic_field_data.x_compensated_ut(),
+                magnetic_field_data.y_compensated_ut(),
+                magnetic_field_data.z_compensated_ut(),
+            );
         }
+        timer.delay_ms(100_u32);
     }
 }
